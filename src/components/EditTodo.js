7810d9fcfa8380a6e58232575f88aa2e9d0978eb
Michel Pereira <michelbecauro@gmail.com>
2021-11-03 03:40:52 -0300

    chore: Implement 'EditTodo' component structure

---
+++ src/components/EditTodo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class EditTodo extends React.Component {
+    // constructor() {
+    //     super();
+    // }
+
+    render() {
+        return <span> I am EditTodo Component </span>
+    }
+}
