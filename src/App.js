c600905b561bb6a93d25b131963d264637d7333b
Michel Pereira <michelbecauro@gmail.com>
2021-11-03 04:54:25 -0300

    feat: Implement 'Navbar' into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import TodoList from './components/TodoList';
 import EditTodo from './components/EditTodo';
 import CreateTodo from './components/CreateTodo';
@@ -8,6 +8,16 @@ import './App.css';
 function App() {
   return (
     <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Todos</Link>
+          </li>
+          <li>
+            <Link to="/create">Create Todo</Link>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path="/" component={TodoList}/>
         <Route exact path="/edit/:id" component={EditTodo}/>
