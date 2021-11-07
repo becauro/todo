edefec6fb5207131d24c25992c1c576adf030f7a
Michel Pereira <michelbecauro@gmail.com>
2021-11-07 03:03:05 -0300

    feat: Implement "navbar" component
    This commit just transform "navbar" a class component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import { Switch, Route, Link } from 'react-router-dom';
 import ListTodo from './pages/ListTodo';
 import UpdateTodo from './pages/UpdateTodo';
 import CreateTodo from './pages/CreateTodo';
+import { NavBar } from "./components/NavBar.js";
 import './App.css';
 
 function App() {
   
   return (
     <div>
-      <nav className="navbar bg-light navbar-expand-lg navbar-light">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item navbar-item-left">
-            <Link to="/" className="nav-link"><span className="nav-link-span">Listar</span></Link>
-          </li>
-          <li className="navbar-item navbar-item-right">
-            <Link to="/create" className="nav-link"><span className="nav-link-span nav-link-span-right" >Criar</span></Link>
-          </li>
-        </ul>
-      </nav>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={ListTodo}/>
         <Route exact path="/edit/:id" component={UpdateTodo}/>
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+export class NavBar extends React.Component {
+
+    render() {
+        return (
+          <nav className="navbar bg-light navbar-expand-lg navbar-light">
+            <ul className="navbar-nav mr-auto">
+            <li className="navbar-item navbar-item-left">
+                <Link to="/" className="nav-link"><span className="nav-link-span">Listar</span></Link>
+            </li>
+            <li className="navbar-item navbar-item-right">
+                <Link to="/create" className="nav-link"><span className="nav-link-span nav-link-span-right" >Criar</span></Link>
+            </li>
+            </ul>
+          </nav>
+        )
+    }
+}
